43d501d7a5e50f82727d733c324319a791400d86
acascoc098 <acascoc098@g.educaand.es>
2024-02-13 11:50:05 +0100

    CRUD categorias empezado

--- src/main/java/com/iesvdc/acceso/zapateria/controladores/CotrollerCategoria.java
+++ src/main/java/com/iesvdc/acceso/zapateria/controladores/CotrollerCategoria.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import com.iesvdc.acceso.zapateria.models.Categoria;
 import com.iesvdc.acceso.zapateria.repos.RepoCategoria;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,17 +21,24 @@ public class CotrollerCategoria {
     RepoCategoria repoCategoria;
 
     @GetMapping("")
-    public String findAll2(Model model) {
-        model.addAttribute(
-            "categorias",
-            repoCategoria.findAll()
-        );
+    public String findAll(Model modelo) {
+        modelo.addAttribute(
+            "categorias", 
+            repoCategoria.findAll());
         return "categorias/categorias";
     }
-
+    
     @GetMapping("/add")
-    public String addUsuario(Model model) {
-        return new String();
+    public String addForm(Model modelo) {
+        modelo.addAttribute("categorias", repoCategoria.findAll());        
+        return "categorias/add";
+    }
+
+    @PostMapping("/add")
+    public String postMethodName(
+        @ModelAttribute("categoria") Categoria categoria)  {
+        repoCategoria.save(categoria);
+        return "redirect:/categorias";
     }
     
     
